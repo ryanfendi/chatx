9835cd3b5bee17b7507b1e0c79c0ae2a42504187
ryanfendi <102475046+ryanfendi@users.noreply.github.com>
2025-08-04 10:28:04 +0700

    main.js

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ const config = {
 const game = new Phaser.Game(config);
 
 function preload() {
-  this.load.image("avatar", "https://i.imgur.com/7ZSMxgq.png");
+  this.load.image("avatar", "https://i.imgur.com/hyNa6eE.png");
 }
 
 function create() {
